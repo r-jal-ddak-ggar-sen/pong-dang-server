aa69016ee1fa12b5bafdeaf1767123ba407b55e8
goodGid <hello.goodgid@gmail.com>
2022-03-05 10:04:03 +0900

    Set Project Structure

---
+++ src/main/java/dev/be/pongdang/PongdangApplication.java
@@ -0,0 +1,13 @@
+package dev.be.pongdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongdangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PongdangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/pongdang/PongdangApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.pongdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongdangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
