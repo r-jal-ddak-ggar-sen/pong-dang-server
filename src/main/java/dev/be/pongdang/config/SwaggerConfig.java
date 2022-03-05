b44b54a8dc136268a6494cbc315d5b317af099c7
goodGid <hello.goodgid@gmail.com>
2022-03-06 06:29:56 +0900

    Modify the order in which the server list is displayed according to the environment

--- src/main/java/dev/be/pongdang/config/SwaggerConfig.java
+++ src/main/java/dev/be/pongdang/config/SwaggerConfig.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final ConfigurableEnvironment env;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -17,6 +22,13 @@ public class SwaggerConfig {
         server.setUrl("https://pongdang.ml");
         Server localhostServer = new Server();
         localhostServer.setUrl("http://localhost:8080");
-        return new OpenAPI().servers(List.of(localhostServer, server));
+
+        // Local 환경
+        List<Server> serverList = List.of(localhostServer, server);
+        if (env.getActiveProfiles().length == 0) { // EC2 환경
+            serverList = List.of(server, localhostServer);
+        }
+
+        return new OpenAPI().servers(serverList);
     }
 }
