e3fc13cdf2f312a3ef9d18eb44856cb8f1ce8e3f
goodGid <hello.goodgid@gmail.com>
2022-03-05 16:58:36 +0900

    Add WebSecurityConfig file

---
+++ src/main/java/dev/be/pongdang/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package dev.be.pongdang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable() // Post 요청 시 403 에러 방지
+                .authorizeRequests().antMatchers("/").permitAll();
+    }
+}
