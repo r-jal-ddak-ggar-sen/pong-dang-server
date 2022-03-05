5a997289add00dc427e84f9c8dff4cdf28e8c328
goodGid <hello.goodgid@gmail.com>
2022-03-05 20:58:52 +0900

    Add field to Member Entity

--- src/main/java/dev/be/pongdang/entity/Member.java
+++ src/main/java/dev/be/pongdang/entity/Member.java
@@ -1,19 +1,28 @@
 package dev.be.pongdang.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member extends BaseEntity {
@@ -28,5 +37,8 @@ public class Member extends BaseEntity {
     private String nickName;
 
     private String password;
+
+    @OneToMany(mappedBy = "makerMember")
+    private List<Pond> makePondList = new ArrayList<>();
 
 }
